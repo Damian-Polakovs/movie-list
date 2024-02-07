1198d6fd4a181ce5b3fac57eaae3dca6ae26e0c3
Damian-Polakovs <s00236491@atu.ie>
2024-02-07 14:28:42 +0000

    Week 3 movie-list complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieService } from './myservices/movie.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'movie-list';
+
+  movies:any[]=[];
+
+  constructor(private movieService:MovieService){}
+
+  ngOnInit(){
+    this.movies=this.movieService.getMovies();
+    console.log(this.movies);
+  }
+  
 }
